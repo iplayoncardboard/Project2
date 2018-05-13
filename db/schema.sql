66c5beed38c11d22b40d0db8b1878f6fb1be787e
unknown <ehurst01@gmail.com>
2018-05-12 19:04:19 -0700

    Added user schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE fablib_db;
 
+USE fablib_db;
+
+CREATE TABLE user(
+    id INTEGER(11) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    user_name VARCHAR(100),
+    pw_hash VARCHAR(255)
+);
