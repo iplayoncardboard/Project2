bf3f676951c6f0880f1f85be3e5656f1b720c10e
unknown <ehurst01@gmail.com>
2018-05-12 21:01:33 -0700

    installed authentication packages. Setting up server

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const passport = require("passport")
+const LocalStrategy = require("passport-local");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+
+//setup express session
+app.use(require("express-session")({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+    
+}));
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
