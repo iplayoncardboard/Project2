548e163712ba337c16c6cb9c7aa7f3a5452bff24
unknown <ehurst01@gmail.com>
2018-05-15 20:16:13 -0700

    Fixed the env stuff and all the crab Brit broke"

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy;
+const session = require('express-session');
 const PORT = process.env.PORT || 8080;
+const env = require('dotenv').config();
+
 
 const app = express();
+
+
+//setup express session
+//WhatThoz30rBsDoTho???
+console.log(process.env.SESSION_SECRET);
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+//Setup passport strategy the db. portion will have to be changed. 
+passport.use(new LocalStrategy(
+    function(username, password, cb) {
+      db.users.findByUsername(username, function(err, user) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        if (user.password != password) { return cb(null, false); }
+        return cb(null, user);
+      });
+    }));
+
+
+    // Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, cb) {
+    db.users.findById(id, function (err, user) {
+      if (err) { return cb(err); }
+      cb(null, user);
+    });
+  });
+  
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
