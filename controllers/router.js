8fed5a9f65e964fc46e1601e89bfc46e45c86f97
unknown <ehurst01@gmail.com>
2018-05-17 20:38:28 -0700

    prep for merge. Added hash and salt...mmmmmm salt.....

--- controllers/router.js
+++ controllers/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
-var Models = require("../models");
-
+const Models = require("../models");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -13,17 +14,42 @@ router.get('/register', (req, res)=> {
 });
 
 router.post('/register', (req, res)=>{
-    
-    let newUser = {
-        user_name: req.body.user_name,
-        pw_hash: req.body.password
-    }
-    
-    Models.user.create(newUser).then((user)=>{
-        res.json(user);
-    });
+   
+  
 
+    Models.user.findAll({
+        where: {
+            user_name: req.body.user_name
+        }
+      }).then((data)=>{
+        if(data.length === 0){
+
+
+            bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+                if(err){
+                    console.log(err);
+                }
+                // Store hash in your password DB.
+                let newUser = {
+                    user_name: req.body.user_name,
+                    pw_hash: hash
+                }
+                Models.user.create(newUser).then((user)=>{
+                    res.json(user);
+                });
+            });
+          
+
+
+           
+        }
+        else {
+            console.log("ERROR USER EXISTS")
+        }
+      });
 });
+
+
 
 
 
