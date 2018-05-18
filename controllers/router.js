71cc653728270c1d8c93f0837f5743287869946c
brittanycflanagan <brittanycturner@gmail.com>
2018-05-17 19:54:42 -0700

    entries form adding to database

--- controllers/router.js
+++ controllers/router.js
@@ -31,6 +31,33 @@ router.get("/play", (req, res) => {
     res.render("play");
 });
 
+router.post('/play', (req, res)=>{
+    let newEntry = {
+        // user_name: req.body.user_name,
+        // pw_hash: req.body.password
+        
+    word_1: req.body.word_1,
+    word_2: req.body.word_2,
+    word_3: req.body.word_3,
+    word_4: req.body.word_4,
+    word_5: req.body.word_5,
+    word_6: req.body.word_6,
+    word_7: req.body.word_7,
+    word_8: req.body.word_8,
+    word_9: req.body.word_9,
+    word_10: req.body.word_10,
+    word_11: req.body.word_11,
+    word_12: req.body.word_12,
+    word_13: req.body.word_13
+    }
+    
+    Models.entries.create(newEntry).then((entries)=>{
+        res.json(entries);
+    });
+
+});
+
+
 
 
 module.exports = router;
--- public/assets/javascript/entries.js
+++ public/assets/javascript/entries.js
@@ -1,16 +1,23 @@
-$(document).on("submit", "#entries-form", handleEntriesFormSubmit);
+// $(document).on("submit", "#entries-form", handleEntriesFormSubmit);
 
- // A function to handle what happens when the form is submitted to create a new Author
- function handleEntriesFormSubmit(event) {
-    event.preventDefault();
-    // Don't do anything if the name fields hasn't been filled out
-    if (!nameInput.val().trim().trim()) {
-      return;
-    }
-    // Calling the upsertAuthor function and passing in the value of the name input
-    upsertAuthor({
-      name: nameInput
-        .val()
-        .trim()
-    });
-  }
+//  // A function to handle what happens when the form is submitted to create a new Author
+//  function handleEntriesFormSubmit(event) {
+//     event.preventDefault();
+//     // Don't do anything if a field hasn't been filled out
+//     if (!word1Input.val().trim().trim()) {
+//       return;
+//     }
+//     // Calling the upsertAuthor function and passing in the value of the name input
+//     upsertEntries({
+//       name: nameInput
+//         .val()
+//         .trim()
+//     });
+
+//     function upsertEntries (userEntries) {
+//         $.post("/api/authors", userEntries)
+//           .then(getAuthors);
+//       }
+//   }
+
+  
